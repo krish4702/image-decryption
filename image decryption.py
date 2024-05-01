051407f123b8f7f97a3022bb008a2d6d4e7c3865
krish4702 <146738656+krish4702@users.noreply.github.com>
2024-05-01 00:48:00 -0700

    Add files via upload

---
+++ image decryption.py
@@ -0,0 +1,17 @@
+path = r'C:\Users\pc\PycharmProjects\pythonProject\OIP.jpeg'
+
+key = int(input('Enter key for decryption of image: '))
+
+print('The path of the file:', path)
+print('The key for decryption:', key)
+
+with open(path, 'rb') as fin:
+    image = bytearray(fin.read())
+
+for index, value in enumerate(image):
+    image[index] = value ^ key
+
+with open(path, 'wb') as fout:
+    fout.write(image)
+
+print('Decryption done.')
